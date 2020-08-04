6669099fa68bacd383ee429343827946697fb2f3
DESKTOP-AOSNOAI\User <zuborev_artem@mail.ru>
2020-08-04 18:27:46 +0300

    task4

--- homework4/script.js
+++ homework4/script.js
@@ -1,15 +1,25 @@
-let btn = document.getElementById('btn');
+
 let dateNow = document.getElementById('now-date');
 let date = new Date();
 dateNow.innerHTML = date;
 
+let btn = document.getElementById('btn');
 btn.onclick = function (event) {
+    let date1 = new Date();
     let name = document.getElementById('name');
-    let text = document.getElementById('text');
-    let comment = document.getElementById('comment');
-    let str = `${name.value} ${date} ${text.value}`;
     console.log(name.value);
-    comment.innerHTML = str;
+    let text = document.getElementById('text');
+    console.log(text.value);
+
+    let str = `<h2>${name.value}</h2><p>${date1}</p><p>${text.value}</p>`;
+    let newDiv = document.createElement('div');
+    newDiv.innerHTML = str;
+    let comment = document.getElementById('comment');
+    comment.prepend(newDiv);
+    name.value = '';
+    text.value = '';
     event.preventDefault();
-    event.stopPropagation();
 }
+
+
+
